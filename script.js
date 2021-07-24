99c58b3b52d76f0d89844dbdfc61ea53a2fff887
Michael Ceballos <ceblance@aol.com>
2021-07-23 20:40:26 -0700

    add js and api key

--- script.js
+++ script.js
@@ -7,7 +7,13 @@ const countryEl = document.getElementById('country');
 const weatherForecastEl = document.getElementById('weather-forecast');
 const currentTempEl = document.getElementById('current-temp');
 
-const days = ['Sunday']
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday',
+'Friday', 'Saturday', 'Sunday']
+const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug',
+'Sep', 'Oct', 'Nov', 'Dec'];
+
+const API_KEY = '43589df45778b5178f575dcb44458988';
+
 setInterval(() => {
     const time = new Date()
     const month = time.getMonth();
@@ -16,11 +22,31 @@ setInterval(() => {
     const hour = time.getHours();
     const hoursIn12HrFormat = hour >= 13 ? hour %12: hour
     const minutes = time.getMinutes();
-    const ampm = hours >=12 ? 'PM' : 'AM'
+    const ampm = hour >=12 ? 'PM' : 'AM'
 
     timeEl.innerHTML = hoursIn12HrFormat + ':' + minutes + ' ' + `<span
     id="am-pm">${ampm}</span>`
 
-    dateEl.innerHTML =
+    dateEl.innerHTML = days[day] + ', ' + date + ' ' + months[month]
 
 }, 1000);
+
+getWeatherData()
+function getWeatherData () {
+    navigator.geolocation.getCurrentPosition((success) => {
+
+        let {latitude, longitude } = success.coords;
+
+        fetch(`api.openweathermap.org/data/2.5/weather?lat=$
+        {latitude}&lon=${longitude}&exclude=hourly,minutely&units=metric&appid=$
+        {API_KEY}`).then(res => res.json()).then(data => {
+        
+             console.log(data);
+             showWeatherData(data);
+        })
+    })
+}
+
+function showWeatherData (data) {
+
+}
